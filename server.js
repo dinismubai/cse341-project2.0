e37bd6be81c8bc9f5ba4b261999cf006652f8c7f
dinismubai <mubydinis@gmail.com>
2025-01-27 17:31:40 +0200

    Fixind swagger issues-2

--- server.js
+++ server.js
@@ -14,16 +14,6 @@ app
         next();
     })
 
-
-/*app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
-    );
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    next();
-});*/
     .use('/', require('./routes'));
 
 process.on('uncaughtException', (err, origin) => {
